35dda88c1d6979ea3a1d7e90969f06a0a4ae1274
Kunal Mundra <kunalmundra2@gmail.com>
2023-10-06 22:09:38 +0530

    Basic structure of app

--- App.js
+++ App.js
@@ -1,26 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = (
-    <h1 id="title" key="h1">
-        Hi Kunal
+const Title = () => (
+    <h1 className="title" key="h1">
+        <a>
+            <img
+                className="logo"
+                alt="logo"
+                src="https://jharkhandbihar.com/productimages/30065.jpg"
+            />
+        </a>
     </h1>
 );
 
 const Header = () => {
     return (
-        <div>
-            {heading}
-            <h1 id="title" key="h1">
-                Hi Kunal
-            </h1>
-            <h2>
-                What is your name?
-            </h2>
+        <div className="header">
+            <Title />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
-
     );
 };
 
+const Body = () => {
+    return (
+        <h4>Body</h4>
+    )
+}
+
+const Footer = () => {
+    return (
+        <h4>Footer</h4>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<AppLayout />);
