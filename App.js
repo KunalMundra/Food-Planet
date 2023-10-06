8a541395d47bc0cdc660ce822344ce0f155d7cb3
Kunal Mundra <kunalmundra2@gmail.com>
2023-10-07 00:09:47 +0530

    HardCoded Data as object, Optional chaininig, Passed as props

--- App.js
+++ App.js
@@ -1,9 +1,600 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+const restaurantList = [
+    {
+        "info": {
+            "id": "14548",
+            "name": "Burger King",
+            "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+            "locality": "Paud Road",
+            "areaName": "Kothrud",
+            "costForTwo": "₹350 for two",
+            "cuisines": [
+                "Burgers",
+                "American"
+            ],
+            "avgRating": 4.1,
+            "feeDetails": {
+                "restaurantId": "14548",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 4200
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 4200
+            },
+            "parentId": "166",
+            "avgRatingString": "4.1",
+            "totalRatingsString": "10K+",
+            "sla": {
+                "deliveryTime": 21,
+                "lastMileTravel": 2.4,
+                "serviceability": "SERVICEABLE",
+                "slaString": "21 mins",
+                "lastMileTravelString": "2.4 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 06:00:00",
+                "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+
+                    }
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "EVERY ITEM",
+                "subHeader": "@ ₹129"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/burger-king-paud-road-kothrud-pune-14548",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "23677",
+            "name": "McDonald's",
+            "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
+            "locality": "Paud Road",
+            "areaName": "Kothrud",
+            "costForTwo": "₹400 for two",
+            "cuisines": [
+                "Burgers",
+                "Beverages",
+                "Cafe",
+                "Desserts"
+            ],
+            "avgRating": 4.3,
+            "feeDetails": {
+                "restaurantId": "23677",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 4200
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 4200
+            },
+            "parentId": "630",
+            "avgRatingString": "4.3",
+            "totalRatingsString": "10K+",
+            "sla": {
+                "deliveryTime": 16,
+                "lastMileTravel": 1.1,
+                "serviceability": "SERVICEABLE",
+                "slaString": "16 mins",
+                "lastMileTravelString": "1.1 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 01:45:00",
+                "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {
+
+            },
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+
+                    }
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/mcdonalds-paud-road-kothrud-pune-23677",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "9377",
+            "name": "Subway",
+            "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+            "locality": "Rahul Nagar",
+            "areaName": "Kothrud",
+            "costForTwo": "₹350 for two",
+            "cuisines": [
+                "Fast Food",
+                "Healthy Food",
+                "Salads",
+                "Snacks",
+                "Desserts",
+                "Beverages"
+            ],
+            "avgRating": 4.2,
+            "feeDetails": {
+                "restaurantId": "9377",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 5000
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 5000
+            },
+            "parentId": "2",
+            "avgRatingString": "4.2",
+            "totalRatingsString": "10K+",
+            "sla": {
+                "deliveryTime": 19,
+                "lastMileTravel": 3,
+                "serviceability": "SERVICEABLE",
+                "slaString": "19 mins",
+                "lastMileTravelString": "3.0 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 00:00:00",
+                "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {
+
+            },
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+
+                    }
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/subway-rahul-nagar-kothrud-pune-9377",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "10841",
+            "name": "Pizza Hut",
+            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+            "locality": "Kothrud",
+            "areaName": "Kothrud",
+            "costForTwo": "₹350 for two",
+            "cuisines": [
+                "Pizzas"
+            ],
+            "avgRating": 3.9,
+            "feeDetails": {
+                "restaurantId": "10841",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 4200
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 4200
+            },
+            "parentId": "721",
+            "avgRatingString": "3.9",
+            "totalRatingsString": "10K+",
+            "sla": {
+                "deliveryTime": 26,
+                "lastMileTravel": 2.8,
+                "serviceability": "SERVICEABLE",
+                "slaString": "26 mins",
+                "lastMileTravelString": "2.8 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 01:00:00",
+                "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+
+                    }
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "50% OFF",
+                "subHeader": "UPTO ₹100"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/pizza-hut-kothrud-pune-10841",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "152984",
+            "name": "Oye Kiddan",
+            "cloudinaryImageId": "usahf2gbff2ybnfnik9n",
+            "locality": "Bharati Nagar",
+            "areaName": "Kothrud",
+            "costForTwo": "₹250 for two",
+            "cuisines": [
+                "Punjabi",
+                "North Indian",
+                "Desserts",
+                "Thalis"
+            ],
+            "avgRating": 4.1,
+            "feeDetails": {
+                "restaurantId": "152984",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 4000
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 4000
+            },
+            "parentId": "690",
+            "avgRatingString": "4.1",
+            "totalRatingsString": "10K+",
+            "sla": {
+                "deliveryTime": 17,
+                "lastMileTravel": 1.4,
+                "serviceability": "SERVICEABLE",
+                "slaString": "17 mins",
+                "lastMileTravelString": "1.4 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 01:00:00",
+                "opened": true
+            },
+            "badges": {
+                "textExtendedBadges": [
+                    {
+                        "iconId": "guiltfree/GF_Logo_android_3x",
+                        "shortDescription": "options available",
+                        "fontColor": "#7E808C"
+                    }
+                ]
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+                        "badgeObject": [
+                            {
+                                "attributes": {
+                                    "description": "",
+                                    "fontColor": "#7E808C",
+                                    "iconId": "guiltfree/GF_Logo_android_3x",
+                                    "shortDescription": "options available"
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "60% OFF",
+                "subHeader": "UPTO ₹120"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/oye-kiddan-bharati-nagar-kothrud-pune-152984",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "243625",
+            "name": "KFC",
+            "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
+            "locality": "Deccan Gymkhana",
+            "areaName": "Deccan Gymkhana",
+            "costForTwo": "₹400 for two",
+            "cuisines": [
+                "Burgers",
+                "Biryani",
+                "American",
+                "Snacks",
+                "Fast Food"
+            ],
+            "avgRating": 4.1,
+            "feeDetails": {
+                "restaurantId": "243625",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 7800
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 7800
+            },
+            "parentId": "547",
+            "avgRatingString": "4.1",
+            "totalRatingsString": "1K+",
+            "sla": {
+                "deliveryTime": 24,
+                "lastMileTravel": 5,
+                "serviceability": "SERVICEABLE",
+                "slaString": "24 mins",
+                "lastMileTravelString": "5.0 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-10-07 01:00:00",
+                "opened": true
+            },
+            "badges": {
+
+            },
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+
+                    },
+                    "textBased": {
+
+                    },
+                    "textExtendedBadges": {
+
+                    }
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "40% OFF",
+                "subHeader": "UPTO ₹80"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {
+
+                    },
+                    "video": {
+
+                    }
+                }
+            },
+            "reviewsSummary": {
+
+            },
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {
+
+            }
+        },
+        "analytics": {
+
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/kfc-deccan-gymkhana-pune-243625",
+            "type": "WEBLINK"
+        }
+    },
+]
+
+
 const Title = () => (
     <h1 className="title" key="h1">
-        <a>
+        <a href="/">
             <img
                 className="logo"
                 alt="logo"
@@ -29,9 +620,33 @@ const Header = () => {
     );
 };
 
+const RestaurantCard = (props) => {
+    console.log(props)
+    return (
+        <div className="Card">
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+                props.restaurant?.info.cloudinaryImageId} /> {/* By default link only Id changes */}
+            <h2>{props.restaurant?.info.name}</h2>  {/* In curly brackets because it is JS not JSX */}
+            <h3>{props.restaurant?.info.cuisines.join(", ")}</h3>
+            <h4>{props.restaurant?.info.avgRatingString} star</h4>
+        </div>
+    )
+}
+
+
 const Body = () => {
     return (
-        <h4>Body</h4>
+        <div className="restaurant-list">
+            <RestaurantCard restaurant={restaurantList[0]}/>
+            {/* {RestaurantCard(restaurantList[0])} 
+                Both are similar (Know the basics - RestaurantCard is just a function and restaurantList[0] is passed as argument)
+            */}
+            <RestaurantCard restaurant={restaurantList[1]} />
+            <RestaurantCard restaurant={restaurantList[2]}  hell0 = "world"/> {/*multiple props sent*/}
+            <RestaurantCard restaurant={restaurantList[3]} />
+            <RestaurantCard restaurant={restaurantList[4]} />
+            <RestaurantCard restaurant={restaurantList[5]} />
+        </div>
     )
 }
 
