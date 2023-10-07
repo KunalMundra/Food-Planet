7e4f43ce79a26d1d702d5e2638d734edec620a5d
Kunal Mundra <kunalmundra2@gmail.com>
2023-10-08 00:50:58 +0530

    Fetched data from swiggy API using useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
-import { restaurantList } from "../config";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
+function filterData(SearchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>
+        restaurant.info.name.includes(SearchText)
+    )
+
+    return filterData;
+}
+
 const Body = () => {
+    const [restaurants, setRestaurants] = useState([]);
     const [SearchText, setSearchText] = useState();
+
+    useEffect(() => {
+
+        //api call
+        getRestaurants();
+    }, [])
+
+    console.log("render");
+
+    async function getRestaurants() {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5116937&lng=73.80349140000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        console.log(json);
+        setRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
 
     return (
         <>
@@ -15,11 +38,16 @@ const Body = () => {
                     value={SearchText}
                     onChange={(e) => setSearchText(e.target.value)}
                 />
-                <button>Search</button>
+                <button
+                    onClick={() => {
+                        const data = filterData(SearchText, restaurants);
+                        setRestaurants(data);
+                    }}
+                >Search</button>
             </div>
             <div className="restaurant-list">
                 {
-                    restaurantList.map((restaurant) => {
+                    restaurants.map((restaurant) => {
                         return <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
                     })
                 }
