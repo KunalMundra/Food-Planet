35b91da1da42f1cb44a8b2f682517039ae222858
Kunal Mundra <kunalmundra2@gmail.com>
2023-10-07 16:53:46 +0530

    Search button added using useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+    const [SearchText, setSearchText] = useState();
+
     return (
-        <div className="restaurant-list">
-            {
-                restaurantList.map((restaurant) => {
-                    return <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
-                })
-            }
-        </div>
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    className="input-container"
+                    placeholder="Search"
+                    value={SearchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+                <button>Search</button>
+            </div>
+            <div className="restaurant-list">
+                {
+                    restaurantList.map((restaurant) => {
+                        return <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
+                    })
+                }
+            </div>
+        </>
     )
 }
 
