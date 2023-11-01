676ce345aa9e11594fea01d37dea78695f662c99
Kunal Mundra <kunalmundra2@gmail.com>
2023-11-01 12:12:47 +0530

    Instamart component created and added using lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -8,6 +8,10 @@ import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Shimmer from "./components/Shimmer";
+
+
+const Instamart = lazy(() => import("./components/Instamart"));
 
 const AppLayout = () => {
     return (
@@ -40,6 +44,12 @@ const approuter = createBrowserRouter([
             {
                 path: "/restaurant/:resId",
                 element: <RestaurantMenu />
+            },
+            {
+                path: "/instamart",
+                element: <Suspense fallback={<Shimmer/>}>
+                            <Instamart />
+                        </Suspense>
             }
         ]
     }
